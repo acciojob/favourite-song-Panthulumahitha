ae975ff9019a9cb975793608b6cbbfd7ac075798
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-12-28 20:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your code here
+document.addEventListener('DOMContentLoaded', function() {
+    const songName = "Bohemian Rhapsody";
+    const youtubeLink = "https://www.youtube.com/watch?v=3JZ4pnNtyxQ";
+    const contentDiv = document.createElement('div');
+    const songParagraph = document.createElement('p');
+    const songAnchor = document.createElement('a');
+
+    songAnchor.href = youtubeLink;
+    songAnchor.textContent = songName;
+    songAnchor.target = "_blank";
+
+    songParagraph.textContent = "My current favourite song is ";
+    songParagraph.appendChild(songAnchor);
+    contentDiv.appendChild(songParagraph);
+
+    document.body.appendChild(contentDiv);
+});
